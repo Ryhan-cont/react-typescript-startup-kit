096f60341bae0d1007010df32b0dcdd59969b56a
ryhan.cont <ryhan.cont@gmail.com>
2023-11-15 01:20:32 +0600

    update

---
+++ src/Router/Route.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
+
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default Routes;
